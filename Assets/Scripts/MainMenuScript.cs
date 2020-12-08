95a264e0901bab01e6d196eb0ecc6633b11ae3a8
Dan <DucatiDan@outlook.com>
2020-12-08 15:15:33 +1300

    Menu

--- Assets/Scripts/MainMenuScript.cs
+++ Assets/Scripts/MainMenuScript.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class MainMenuScript : MonoBehaviour
 
 {
 
-    public void Start()
+    public void Game()
     {
         SceneManager.LoadScene("GameScene");
     }
